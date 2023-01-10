72feaca9cd042225575768612999c2e67bacb436
dmitryjob4j <haoos@inbox.ru>
2023-01-10 16:00:29 +0300

    4. Просмотр, редактирование и удаление вакансии [#504850 #282679]

--- src/main/java/ru/my/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/my/dreamjob/controller/VacancyController.java
@@ -2,10 +2,7 @@ package ru.my.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.my.dreamjob.model.Vacancy;
 import ru.my.dreamjob.repository.MemoryVacancyRepository;
 import ru.my.dreamjob.repository.VacancyRepository;
@@ -43,6 +40,37 @@ public class VacancyController {
     @PostMapping("/create")
     public String createVacancy(@ModelAttribute Vacancy vacancy) {
         vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
+    }
+
+    @GetMapping("/{id}")
+    public String getByIdVacancy(Model model, @PathVariable int id) {
+        var vacancyOptional = vacancyRepository.findById(id);
+        if (vacancyOptional.isEmpty()) {
+            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        model.addAttribute("vacancy", vacancyOptional.get());
+        return "vacancies/one";
+    }
+
+    @PostMapping("/update")
+    public String updateVacancy(@ModelAttribute Vacancy vacancy, Model model) {
+        var isUpdate = vacancyRepository.update(vacancy);
+        if (!isUpdate) {
+            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        return "redirect:/vacancies";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        var isDeleted = vacancyRepository.deleteById(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
         return "redirect:/vacancies";
     }
 }
